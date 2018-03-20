79f2024b10e70b1d5fb283e17f036de526f8c33e
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-19 20:51:05 -0700

    fifth commit with comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,25 @@
+// Variable declarations
+
 var owner = process.argv[2];
 var repo = process.argv[3];
 var request = require('request');
 var secrets = require('./secrets.js');
+
+
+// Returns an error message if no owner or repositories specified
 
 if(!owner || !repo){
   console.log("Error! Please enter at least two values.");
   return;
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+// Introductory program message
+
+console.log('Welcome to the GitHub Avatar Downloader! Downloading avatars...');
+
+
+// Fetches corresponding repository data
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -19,11 +30,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+
+// Parses string to object using JSON.parse
+
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
 }
 
+
+// Downloads avatars and saves to corresponding filepaths
 
  function downloadImageByURL(url, filePath){
 
@@ -33,6 +49,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
+
+// Fetches individual avatar urls
 
 getRepoContributors(owner, repo, function(err, result) {
   for(var i = 0; i < result.length; i++){
